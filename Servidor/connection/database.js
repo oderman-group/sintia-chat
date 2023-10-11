a62e1087baa0cc5782fd2c1f7483b07f2d046fa7
Admin Softcomputo <softcomputo77@gmail.com>
2023-10-11 10:59:39 -0500

    pes2023-390 configurar conexion

--- Servidor/connection/database.js
+++ Servidor/connection/database.js
@@ -1,14 +1,19 @@
 import mysql from 'promise-mysql';
 import config from '../config.js';
 
-const connection = mysql.createConnection({
-    host: config.host,
-    database: config.database,
-    user: config.username,
-    password: config.password
-});
 
 
-export function getConnection() {
+const getConnection=async (req, res) =>{
+   const  connection = mysql.createConnection({
+        host: config.host,
+        database: config.database,
+        user: config.username,
+        password: config.password
+    });
     return connection;
-  }
+}
+
+
+export const methods = {
+    getConnection
+};
