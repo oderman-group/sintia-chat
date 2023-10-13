cded5a2bb654935568c0973096dc0216f14aac79
Admin Softcomputo <softcomputo77@gmail.com>
2023-10-13 10:28:24 -0500

    pes2023-389 cambios de emision

--- Servidor/controller/chat.controller.js
+++ Servidor/controller/chat.controller.js
@@ -1,5 +1,22 @@
 const connectiondb = require('../connection/database.js');
 
+const actualizarVisto = async (req, res) =>{
+    try {
+        let { chat_id } = req;
+        if (chat_id === undefined) {
+            res.status(400).json({ result: " Los campos de envios no estan completos" });
+        }
+        const connection = await connectiondb.getConnection();
+        const result = await connection.query("UPDATE mobiliar_sintia_social.chat SET chat_visto = 0 WHERE chat_id =" + chat_id);
+        console.log(`se actualizo el chat con id ${chat_id}`);   
+        connection.end();
+    } catch (error) {
+        console.log(error);
+        res.status(500);
+        res.send(error.message);
+    }
+}
+
 const insertMessageCaht = async (req, res) =>{
     try {
         let { chat_mensaje, chat_remite_usuario, chat_destino_usuario,chat_tipo,chat_url_file } = req;
@@ -11,8 +28,9 @@ const insertMessageCaht = async (req, res) =>{
         }
         const connection = await connectiondb.getConnection();
         const result = await connection.query("INSERT INTO mobiliar_sintia_social.chat(chat_remite_usuario,chat_destino_usuario,chat_mensaje,chat_tipo,chat_url_file)"+ 
-                                               "VALUES ('" + chat_remite_usuario + "','" + chat_destino_usuario + "','" + chat_mensaje + "','" + chat_tipo + "','" + chat_url_file + "')");
+                                               "VALUES ('" + chat_remite_usuario + "','" + chat_destino_usuario + "','" + chat_mensaje + "','" + chat_tipo + "','" + chat_url_file + "') RETURNING chat_id");
         connection.end();
+        return result[0]["chat_id"];
     } catch (error) {
         console.log(error);
         res.status(500);
@@ -27,18 +45,12 @@ const contarNotificaciones = async (req, res) =>{
         }
         const connection = await connectiondb.getConnection();
         const result = await connection.query("SELECT COUNT(*) AS cantidad FROM mobiliar_sintia_social.chat WHERE " +
-        "chat_remite_usuario = '" + chat_destino_usuario + "' AND  chat_destino_usuario = '" + chat_remite_usuario + "' AND " +
-        "chat_visto = 0 "+
-        "ORDER BY chat_fecha_registro ASC",function(error,results,fields9){
-                if(error){
-                    throw error
-                }
-                
-                results.forEach(element => {
-                    console.log(element);
-                });
-        });    
+        "chat_remite_usuario = '" + chat_remite_usuario + "' AND  chat_destino_usuario = '" + chat_destino_usuario + "' AND " +
+        "chat_visto = 1 "+
+        "ORDER BY chat_fecha_registro ASC");    
+        console.log(`mensajes sin leer de ${chat_destino_usuario} (${result[0]["cantidad"]})`);        
         connection.end();
+        return result[0]["cantidad"];
     } catch (error) {
         console.log(error);
         res.status(500);
@@ -53,7 +65,7 @@ const  getlistarChat= async (req, socket) =>{
         "(chat_remite_usuario = '" + chat_remite_usuario + "' AND  chat_destino_usuario = '" + chat_destino_usuario + "') OR " +
         "(chat_remite_usuario = '" + chat_destino_usuario + "' AND  chat_destino_usuario = '" + chat_remite_usuario + "')" +
         "ORDER BY chat_fecha_registro ASC");
-        console.log(`listando en  listar_mensajes_chat${chat_remite_usuario}  el resultado de ('${result.length}') converasaciones`);
+        console.log(`listando en  listar_mensajes_chat ${chat_remite_usuario}  el resultado de ('${result.length}') converasaciones`);
         socket.emit('listar_chat_'+chat_remite_usuario+'_'+chat_destino_usuario, { 
             status:"OK",
             count:result.length,
@@ -65,5 +77,6 @@ const  getlistarChat= async (req, socket) =>{
 module.exports = {
     insertMessageCaht,
     contarNotificaciones,
-    getlistarChat
+    getlistarChat,
+    actualizarVisto
 };
--- Servidor/sockets.js
+++ Servidor/sockets.js
@@ -1,76 +1,90 @@
 const SocketIo = require('socket.io');
-const  { getlistarChat ,insertMessageCaht,contarNotificaciones} = require('./controller/chat.controller.js');
+const { getlistarChat,
+  insertMessageCaht,
+  contarNotificaciones,
+  actualizarVisto } = require('./controller/chat.controller.js');
 
 function configureSocketIO(server) {
   const io = SocketIo(server);
-  io.on("connection", (socket) => {
-    console.log("usuario conectado con Skect id: " + socket.id);
 
-    socket.on('join', (salaChat) => {
-      socket.join(salaChat); // El nombre de usuario se usa como el nombre de la sala
-      console.log(`se creó la sala ${salaChat}`);
-    });
-    socket.on('leave', (salaChat) => {
-      socket.leave(salaChat);
-      console.log(`Usuario dejó la sala '${salaChat}'`);
-    });
+  io.on("connection", (socket) => {// se contrala la coneccion realizada desde el clinete
+    console.log(socket.id + " usuario conectado");
 
-    
-    socket.on("listar_mensajes_chat", (body) => {
-      console.log("recibi un mensaje del id: " + socket.id + " para litar chat de la sala:" + body["salaChat"]);
-      let salaChat = body["salaChat"];
-      if (io.sockets.adapter.rooms.has(salaChat)) {
-        console.log(`listando chat de sala '${salaChat}'`);
-        getlistarChat(body,socket);
-      } 
-    });
-
-    socket.on("enviar_mensaje_chat", (body) => {
-      // console.log("recibi un mensaje del id: " + socket.id + " message:  " + body["chat_mensaje"] + " - para la sala del chat:" + body["salaChat"] + " y sala " + body["sala"]);
-      insertMessageCaht(body);
+    socket.on("enviar_mensaje_chat", async (body) => {
+      chat_id=await insertMessageCaht(body);
+      body["chat_id"] = chat_id;
       let salaChat = body["salaChat"];
       let sala = body["sala"];
-      if (io.sockets.adapter.rooms.has(salaChat)) {
-        console.log(`Sala '${salaChat}' existe.`);
-        io.to(body["salaChat"]).emit('nuevo_mensaje_chat', {
-          body,
-          from: socket.id.slice(8),
-        });
-      } else {
-        console.log(`Sala '${salaChat}' no existe.`);
-        console.log(`se enviara la notificacion a la sala  ${sala} `);
-        if (io.sockets.adapter.rooms.has(sala)) {
-          io.to(sala).emit('notificacion_chat', body);
-          contarNotificaciones(body,socket);
-        }else{
-          console.log(`no se envia a la Sala '${sala}' por que tampoco existe.`);
-        }
-        
-
-      }
-      // socket.broadcast.emit(body["salaChat"], {
-      //     body,
-      //     from: socket.id.slice(8),
-      // });
+      let miSala = body["miSala"];
+      console.log(socket.id + " envio a la sala chat :" + salaChat);
+      socket.broadcast.emit(salaChat, { body, from: socket.id.slice(8), });
+      console.log(socket.id + ` me envio el mensaje a mi propia sala:  '${miSala}'.`);
+      io.emit(miSala, body);
+      console.log(socket.id + " envia a la sala: " + salaChat);
+      var chat_visto = await contarNotificaciones(body, socket);// consulto cuantos visto tiene el susuario para notificar
+      body["cantidad"] = chat_visto;// lo asigno al body para enviarlo
+      socket.broadcast.emit(sala, body);
     });
 
-
-    socket.on("conectar", (body) => {
-      console.log("se coencto un usuario socket id: " + socket.id);
-
+    socket.on("ver_mensaje", async (body) => {
+      await actualizarVisto(body);
     });
 
-    socket.on("desconectar", (body) => {
-      console.log("se descoencto un usuario socket id: " + socket.id);
-
+    socket.on("listar_mensajes_chat", (body) => {// escuhamos pedido para listar las conversaciones 
+      getlistarChat(body, socket);
     });
+    // socket.on('join', (salaChat) => { // escuhamos las salas que se estan creando
+    //   socket.join(salaChat);
+    //   console.log(`+ se creó la sala ${salaChat}`);
+    // });
 
+    // socket.on('leave', (salaChat) => { // escuhamos las salas que se estan cerrando
+    //   socket.leave(salaChat);
+    //   console.log(`x  se cerro la sala '${salaChat}'`);
+    // });
+
+    // socket.on("listar_mensajes_chat", (body) => {// escuhamos pedido para listar las conversaciones 
+    //   let salaChat = body["salaChat"];
+    //   if (io.sockets.adapter.rooms.has(salaChat)) {// validamos que la sala exista
+    //     getlistarChat(body, socket);// emitimos las convesaciones de esa sala
+    //   }
+    // });
+
+    // socket.on("enviar_mensaje_chat", async (body) => {    // escuhamos cuando nos envian un mensaje    
+    //   await insertMessageCaht(body);// insertamos lso datos en la base de datos
+    //   let salaChat = body["salaChat"];
+    //   let sala = body["sala"];
+    //   let miSala = body["miSala"];
+    //   console.log(`me envio el mensaje a mi propia sala  '${miSala}'.`);
+    //   io.to(miSala).emit('mi_notificacion_chat', body); //intentamos emitirnos nuestro mensaje
+    //   if (io.sockets.adapter.rooms.has(salaChat)) {// validamos que la sala esete abierta para enviar el mensaje recibido
+    //     console.log(`Sala chat '${salaChat}' existe.`);
+    //     io.to(body["salaChat"]).emit('nuevo_mensaje_chat', {// si exite la sala emitimos como nuevo mensaje la sala existente
+    //       body,
+    //       from: socket.id.slice(8),
+    //     });
+    //   } else {
+    //     console.log(`Sala chat '${salaChat}' no existe.`);
+    //     console.log(`se enviara la notificacion a la sala  ${sala} `);
+    //     if (io.sockets.adapter.rooms.has(sala)) {// si no existe la enviamos la sala general del usuario 
+    //       var chat_visto=await contarNotificaciones(body, socket);// consulto cuantos visto tiene el susuario para notificar
+    //       body["cantidad"]=chat_visto;// lo asigno al body para enviarlo
+    //       io.to(sala).emit('notificacion_chat', body);
+
+    //     } else {
+    //       console.log(`No se envia a la Sala '${sala}' por que tampoco existe.`);// no hacemos nada si el usuario no esta en linea
+    //     }
+    //   }
+    // });
+    // socket.on("conectar", (body) => {
+    //   console.log("se coencto un usuario socket id: " + socket.id);
+    // });
+    // socket.on("desconectar", (body) => {
+    //   console.log("se descoencto un usuario socket id: " + socket.id);
+    // });
     socket.on("disconnect", (body) => {
       console.log("se descoencto un usuario socket id: " + socket.id);
-
     });
-
-
 
   });
 }
