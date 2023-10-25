bd7c3767a2547fa65a0ae1ca4401cfe38200161d
Admin Softcomputo <softcomputo77@gmail.com>
2023-10-25 11:40:34 -0500

    pes2023-390 eliminacion de index y funciones que no se utilizan

--- Servidor/sockets.js
+++ Servidor/sockets.js
@@ -26,6 +26,7 @@ function configureSocketIO(server) {
       body["cantidad"] = chat_visto;// lo asigno al body para enviarlo
       socket.broadcast.emit(sala, body);
       var chat_mis_vistos = await contarMisNotificaciones(body, socket);// consulto cuantos visto tiene el susuario para notificar
+      console.log(socket.id + " se notifica a la sala: " + sala);
       socket.broadcast.emit("notificacion_"+sala, chat_mis_vistos);
     });
 
@@ -44,58 +45,7 @@ function configureSocketIO(server) {
       socket.emit("notificacion_"+miSala, chat_mis_vistos);
     });
 
-    socket.on("listar_mensajes_chat", (body) => {// escuhamos pedido para listar las conversaciones 
-      getlistarChat(body, socket);
-    });
-    // socket.on('join', (salaChat) => { // escuhamos las salas que se estan creando
-    //   socket.join(salaChat);
-    //   console.log(`+ se creó la sala ${salaChat}`);
-    // });
-
-    // socket.on('leave', (salaChat) => { // escuhamos las salas que se estan cerrando
-    //   socket.leave(salaChat);
-    //   console.log(`x  se cerro la sala '${salaChat}'`);
-    // });
-
-    // socket.on("listar_mensajes_chat", (body) => {// escuhamos pedido para listar las conversaciones 
-    //   let salaChat = body["salaChat"];
-    //   if (io.sockets.adapter.rooms.has(salaChat)) {// validamos que la sala exista
-    //     getlistarChat(body, socket);// emitimos las convesaciones de esa sala
-    //   }
-    // });
-
-    // socket.on("enviar_mensaje_chat", async (body) => {    // escuhamos cuando nos envian un mensaje    
-    //   await insertMessageCaht(body);// insertamos lso datos en la base de datos
-    //   let salaChat = body["salaChat"];
-    //   let sala = body["sala"];
-    //   let miSala = body["miSala"];
-    //   console.log(`me envio el mensaje a mi propia sala  '${miSala}'.`);
-    //   io.to(miSala).emit('mi_notificacion_chat', body); //intentamos emitirnos nuestro mensaje
-    //   if (io.sockets.adapter.rooms.has(salaChat)) {// validamos que la sala esete abierta para enviar el mensaje recibido
-    //     console.log(`Sala chat '${salaChat}' existe.`);
-    //     io.to(body["salaChat"]).emit('nuevo_mensaje_chat', {// si exite la sala emitimos como nuevo mensaje la sala existente
-    //       body,
-    //       from: socket.id.slice(8),
-    //     });
-    //   } else {
-    //     console.log(`Sala chat '${salaChat}' no existe.`);
-    //     console.log(`se enviara la notificacion a la sala  ${sala} `);
-    //     if (io.sockets.adapter.rooms.has(sala)) {// si no existe la enviamos la sala general del usuario 
-    //       var chat_visto=await contarNotificaciones(body, socket);// consulto cuantos visto tiene el susuario para notificar
-    //       body["cantidad"]=chat_visto;// lo asigno al body para enviarlo
-    //       io.to(sala).emit('notificacion_chat', body);
-
-    //     } else {
-    //       console.log(`No se envia a la Sala '${sala}' por que tampoco existe.`);// no hacemos nada si el usuario no esta en linea
-    //     }
-    //   }
-    // });
-    // socket.on("conectar", (body) => {
-    //   console.log("se coencto un usuario socket id: " + socket.id);
-    // });
-    // socket.on("desconectar", (body) => {
-    //   console.log("se descoencto un usuario socket id: " + socket.id);
-    // });
+ 
     socket.on("disconnect", (body) => {
       console.log("se descoencto un usuario socket id: " + socket.id);
     });
--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const server = require('./Servidor/app.js');
-const { PORT } = require('./Servidor/config.js');
+const { PORT, BD_SOCIAL } = require('./Servidor/config.js');
 
  
 server.listen(PORT);
-console.log(`El servidor inicio en el puerto ${PORT}`);
+console.log(`El servidor inicio en el puerto ${PORT} conentandose en la BD ${BD_SOCIAL}`);
 
