2b0cbdb2d6ac0ff6ce9fb174ed23147b921c79b9
Luis Guillermo <softcomputo77@gmail.com>
2024-10-11 14:33:19 -0500

    PES-1112 actualizar index.js

--- index.js
+++ index.js
@@ -1,7 +1,37 @@
-const server = require('./Servidor/app.js');
-const { PORT, BD_SOCIAL } = require('./Servidor/config.js');
+const server = require('./Servidor/app.js')
+const { PORT } = require('./Servidor/config.js');
+const os = require('os');
+const express = require('express');
+var http = require('http');
 
- 
-server.listen(PORT);
-console.log(`El servidor inicio en el puerto ${PORT} conentandose en la BD ${BD_SOCIAL}`);
 
+
+// Obtener la hora actual
+const obtenerHoraActual = () => {
+    const fecha = new Date();
+    return fecha.toLocaleString(); // Muestra fecha y hora en formato legible
+  };
+
+
+// Función para obtener la IP de la máquina donde está corriendo el servidor
+const getServerIp = () => {
+    const networkInterfaces = os.networkInterfaces();
+    for (const interfaceName in networkInterfaces) {
+      const addresses = networkInterfaces[interfaceName];
+      for (const address of addresses) {
+        if (address.family === 'IPv4' && !address.internal) {
+          return address.address;
+        }
+      }
+    }
+    return 'localhost'; // Si no se encuentra, devolver 'localhost'
+  };
+  
+const serverIp = getServerIp();
+
+server.listen(PORT, () => {
+    console.log(`El servidor está corriendo en: http://${serverIp}:${PORT}`);
+    console.log(`El servidor inicio en el puerto ${PORT} `);
+    console.log(`Hora de despliegue: ${obtenerHoraActual()}`);
+  });
+  
