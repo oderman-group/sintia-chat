ec02cd1b2c20c05aae00c85ce254b6ec72d3d0f5
Luis Guillermo <softcomputo77@gmail.com>
2024-10-11 14:36:07 -0500

    PES-1112 corregir routes

--- Servidor/routes/chat.routers.js
+++ Servidor/routes/chat.routers.js
@@ -1,10 +1,6 @@
 import { Router } from "express";
-import { methods  as metodosChat} from "../controller/chat.controller.js";
 
 const router = new Router();
 
-//router.get('/chat/find/:chat_remite_usuario/:chat_destino_usuario', metodosChat.listarChat);
-// router.post('/chat/insert', metodosChat.insertarMensaje);
 
-
-export default router;
+module.exports = router;
